0baf7dd16833673f1f33623f0ef3688ccb0e903f
Harlan Haskins <harlan@valleyrocket.com>
2012-08-17 14:33:04 -0400

    Initial Commit

---
+++ Elian/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Elian
+//
+//  Created by Harlan Haskins on 8/17/12.
+//  Copyright (c) 2012 Valley Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Elian/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Elian
+//
+//  Created by Harlan Haskins on 8/17/12.
+//  Copyright (c) 2012 Valley Rocket. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
